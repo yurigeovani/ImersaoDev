c25caa86800ebd0c05a53581506e952c20cf860b
Yuri <yuri_geovani@hotmail.com>
2021-10-31 02:52:54 +0000

    Updates

--- aulas/aula04/04.js
+++ aulas/aula04/04.js
@@ -1,8 +1,18 @@
-var listFilms = [];
+let novoFilme = document.getElementById("novoFilme");
+
+novoFilme.addEventListener("keyup", function(e){
+  if(e.keyCode ===13){
+    e.preventDefault();
+    document.getElementById("btnAdd").click();
+  }
+})
 
 function adicionar(){
-  listFilms.push("<img src=\"" + document.getElementById("novoFilme").value + "\">");
-  for(let i in listFilms){
-    document.getElementById("listFilms").innerHTML = listFilms;
-  }
+
+  let elementNovoFilme = "<img src=\"" + novoFilme.value + "\" class=\"film\">";
+  let listaFilme = document.getElementById("listFilms");
+  
+  listaFilme.innerHTML += elementNovoFilme;
+
+  document.getElementById("novoFilme").value = "";
 }
--- main.js
+++ main.js
@@ -2,6 +2,3 @@ function playVideo (e) {
     const video = document.getElementById("divVideo");
     video.innerHTML = "<video src=\"/videos/" + e + ".mp4\" controls autoplay></video>";
 }
-
-
-
